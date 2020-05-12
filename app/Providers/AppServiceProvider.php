190a54d3232a19d797205e366713c8c7468df7cf
takuya katsusa <katsusa.takuya@gmail.com>
2020-05-12 18:19:48 +0900

    コメント

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,9 +25,4 @@ class AppServiceProvider extends ServiceProvider
     {
          Schema::defaultStringLength(191);
     }
-
-    public function boot(UrlGenerator $url)
-    {
-    $url->forceScheme('https');
-    } 
 }
